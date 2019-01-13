6113b09ac981dd78fb3896001dbfab0c10d87480
iwong <iwhongy@gmail.com>
2019-01-13 13:16:06 +0800

    change index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! from mr31');
+  res.send('Hello World! from mr30');
 });
 
 var server = app.listen(3000, function () {
