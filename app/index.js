970ca059a31a61500a2a7d12ac56b61162e82bb1
iwong <iwhongy@gmail.com>
2019-01-26 17:12:32 +0800

    Node JS #2

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! from idon');
+  res.send('Hello World! from idon#2');
 });
 
 var server = app.listen(3000, function () {
